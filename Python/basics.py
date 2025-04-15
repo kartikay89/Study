86d45234a917d3442aa4c7d55a9de0b095888045
Kartikay Singh <kartikays89@gmail.com>
2025-04-15 21:42:34 +0200

    Study: Python basics

---
+++ Python/basics.py
@@ -0,0 +1,16 @@
+"""
+Description: Script to practice Python basics.
+Author: Kartikay Singh
+"""
+# Hello World
+print("Hello World!")
+
+# Checking the version of Python through sys library
+import sys
+print("Python version is: ", sys.version)
+
+# Variables
+x = 5
+print(x)
+y = "Kartikay"
+print(y)
